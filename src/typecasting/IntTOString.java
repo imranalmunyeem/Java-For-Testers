48d1d29b91b5818d39daeed3b1ec1b7f636f2703
IMRAN AL MUNYEEM <ialmunyeem@gmail.com>
2022-09-25 02:25:27 +0600

    Conversion

---
+++ src/typecasting/IntTOString.java
@@ -0,0 +1,15 @@
+// Type conversion from int to String
+
+package typecasting;
+
+public class IntTOString {
+	  public static void main(String[] args) {
+		    // create int type variable
+		    int num = 10;
+		    System.out.println("The integer value is: " + num);
+
+		    // converts int to string type
+		    String data = String.valueOf(num);
+		    System.out.println("The string value is: " + data);
+		  }
+}
